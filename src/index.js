9d1e860773a8cb150a85f6f9fbaaab4f034b1fe0
lekan <laksidesvibe@gmail.com>
2023-04-16 22:43:54 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const inputField = document.getElementById("new-task-description");
+
+  const formElement = document.querySelector("form");
+  
+  formElement.addEventListener('submit', (e) => {
+  let newtask =inputField.value
+
+    e.preventDefault();
+    if (newtask.length===0){
+    alert('Task cannot be empty')
+    }else{
+      handleToDo(newtask);
+    }
+    formElement.reset()
+  });
+
+
 });
